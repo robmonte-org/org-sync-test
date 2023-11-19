27d91e269a4de91f74de18a6c3228418097dc8b2
robmonte <17119716+robmonte@users.noreply.github.com>
2023-11-18 22:41:01 -0600

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/robmonte/ghas-test-sync
 
 go 1.21.4
 
-require github.com/robmonte-org/org-private-printer v0.0.0-20231119035747-6d9771ca9b18
+require (
+	github.com/robmonte-org/org-private-printer v0.0.0-20231119035747-6d9771ca9b18
+	github.com/robmonte/private-printer-restricted v0.4.0
+)
--- print.go
+++ print.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 
 	"github.com/robmonte-org/org-private-printer/privateprinter"
+	"github.com/robmonte/private-printer-restricted/privateprinterrestricted"
 )
 
 const INCREMENT_ME int = 1
 
 func Print() {
 	privateprinter.PrivatePrint(fmt.Sprintf("dependabot test with access %d", INCREMENT_ME))
+	privateprinterrestricted.PrivatePrint(fmt.Sprintf("dependabot test with access %d", INCREMENT_ME))
 }
